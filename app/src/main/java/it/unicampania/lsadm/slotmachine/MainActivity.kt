e07758051ed05b534cf261a18a63eb08335119f0
Pasquale Cantiello <pasquale.cantiello@unicampania.it>
2019-04-16 12:35:25 +0200

    Letto il valore di forza ed escursione della leva

--- app/src/main/java/it/unicampania/lsadm/slotmachine/MainActivity.kt
+++ app/src/main/java/it/unicampania/lsadm/slotmachine/MainActivity.kt
@@ -1,12 +1,49 @@
 package it.unicampania.lsadm.slotmachine
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * LSADM 2019 - Slot Machine
+ * Esempio di creazione task asincroni
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Imposta la seekbar
+        seekLeva.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+
+            var start: Long = 0
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                // Non ci interessa
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                start = System.currentTimeMillis()  // Memorizzo il tempo di inizio
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                val durata = System.currentTimeMillis() - start
+                val escursione = seekBar?.progress ?: 0
+                nuovoLancio(durata, escursione)
+            }
+        })
     }
+
+    /**
+     * Effettua un lancio
+     * durata = tempo in ms impiegato per abbassare la leva
+     * escursione = escursione raggiunta (0-100)
+     */
+    fun nuovoLancio(durata: Long, escursione: Int) {
+        // Qui devo invocare task asincrono che muoverà le rotelle
+    }
+
 }
